25dcaf65d3c0c091da7c35b9d568e84b260ef396
ivan <i180388@yandex.ru>
2025-12-15 21:03:38 +0300

    rev.1.07 Устранены замечания после ревью

--- src/ru/yandex/practicum/Clue.java
+++ src/ru/yandex/practicum/Clue.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Clue {
-    private static ArrayList<String> clue = new ArrayList<>();
+    private ArrayList<String> clue = new ArrayList<>();
 
-    Random random = new Random();
+    private Random random = new Random();
 
 
     public ArrayList<String> getClue() {
@@ -21,138 +21,131 @@ public class Clue {
         this.clue = clue;
     }
 
-    public static void listOfWords(String word, char[] charArray) {
-        char[] charClue = word.toCharArray();           //слово
-        char[] charClue1 = charArray;                   //служебные символы
-        ArrayList<String> clue1 = new ArrayList<>();    //временный список
-        ArrayList<String> clue2 = new ArrayList<>();    //временный список 2
-        ArrayList<String> clue3 = new ArrayList<>();    //временный список 3
+    public void listOfWords(String word, char[] charArray) {
+        char[] wordSpelled = word.toCharArray();                //слово
+        char[] serviceSymbols = charArray;                   //служебные символы
 
-        int numberOfGuessed = 0;
-        int numberOfGuessed2 = 0;
-        int numberOfGuessed3 = 0;
-        for (char char1 : charClue1) {
-            if (char1 == '+') {
-                numberOfGuessed++;
+        int numberOfGreen = 0;                              // отгаданы
+        int numberOfYellow = 0;                             // есть в слове, но не на своем месте
+        int numberOfGrays = 0;                              // нет в слове
+
+        for (char ch : serviceSymbols) {
+            if (ch == '+') {
+                numberOfGreen++;
             }
-        }
-        for (char char1 : charClue1) {
-            if (char1 == '^') {
-                numberOfGuessed2++;
+            if (ch == '^') {
+                numberOfYellow++;
             }
-        }
-        for (char char1 : charClue1) {
-            if (char1 == '-') {
-                numberOfGuessed3++;
+            if (ch == '-') {
+                numberOfGrays++;
             }
         }
 
-        try {
-            for (String words : clue) {
-                char[] charClue2 = words.toCharArray();               //слово из списка
-                int numberOfGuessed1 = 0;                             //+
+        clue = ChoiceByGreen(wordSpelled, serviceSymbols, numberOfGreen);
+        clue = ChoiceByYellow(wordSpelled, serviceSymbols, numberOfYellow);
+        clue = ChoiceByGrays(wordSpelled, serviceSymbols, numberOfGrays, numberOfGreen);
+    }
 
-                for (int i = 0; i < word.length(); i++) {
-                    if (charClue2[i] == charClue[i] && charClue1[i] == '+') {
-                        numberOfGuessed1++;
-                    }
-                }
+    private ArrayList<String> ChoiceByGreen(char[] wordSpelled, char[] serviceSymbols, int numberOfGreen) {
 
-                if (numberOfGuessed1 == numberOfGuessed) {
-                    clue1.add(new String(charClue2));
+        ArrayList<String> wordsOfGreen = new ArrayList<>();       //временный список для слов прошедших по +
+
+        for (String words : clue) {
+            char[] wordFromTheList = words.toCharArray();               //слово из списка
+            int numberOfGreen1 = 0;                                     //+
+
+            for (int i = 0; i < words.length(); i++) {
+                if (wordFromTheList[i] == wordSpelled[i] && serviceSymbols[i] == '+') {
+                    numberOfGreen1++;
                 }
             }
-        } catch (NullPointerException e) {
-            System.err.println("Произошла ошибка NullPointer.");
-        } catch (IndexOutOfBoundsException e) {
-            System.err.println("Произошла ошибка выхода за границы массива.");
+
+            if (numberOfGreen1 == numberOfGreen) {
+                wordsOfGreen.add(new String(wordFromTheList));
+            }
         }
 
-        clue = clue1;
         Wordle.printWriter.writeData("Создан список возможных вариантов ответов(по \"+\"): "
-                + clue1.size() + " слов.");
+                + wordsOfGreen.size() + " слов.");
 
-        try {
+        return wordsOfGreen;
+    }
 
-            char[] charClue4 = new char[numberOfGuessed2];     //проверяемые символы на вхождение
-            int cloud = 0;
+    private ArrayList<String> ChoiceByYellow(char[] wordSpelled, char[] serviceSymbols, int numberOfYellow) {
 
-            for (int i = 0;  i < charClue1.length; i++) {
-                if (charClue1[i] == '^') {
-                    charClue4[cloud] = charClue[i];
-                    cloud++;
-                }
+        ArrayList<String> wordsOfYellow = new ArrayList<>();         //временный список проверенных по ^
+        char[] LettersToCheck = new char[numberOfYellow];            //проверяемые символы на вхождение
+        int cloud = 0;
+
+        for (int i = 0; i < serviceSymbols.length; i++) {
+            if (serviceSymbols[i] == '^') {
+                LettersToCheck[cloud] = wordSpelled[i];
+                cloud++;
             }
-
-
-            for (String words : clue) {
-                char[] charClue5 = words.toCharArray();                 //слово из списка
-                for (int i = 0; i < charClue5.length; i++) {
-                    if (charClue1[i] == '+') {
-                        charClue5[i] = '+';
-                    }
-                }
-                if (containsAllChars(charClue5, charClue4)) {
-                    clue2.add(words);
-                }
-            }
-
-            clue = clue2;
-            Wordle.printWriter.writeData("Создан список возможных вариантов ответов(по \"+\" и \"^\"): "
-                    + clue2.size() + " слов.");
-
-        } catch (NullPointerException e) {
-            System.err.println("Произошла ошибка NullPointer2.");
-        } catch (IndexOutOfBoundsException e) {
-            System.err.println("Произошла ошибка выхода за границы массива2.");
         }
 
-        try {
-            char[] charClue6 = new char[numberOfGuessed3];                      //символы которых нет
-
-            int cloud = 0;
-
-            for (int i = 0;  i < charClue1.length; i++) {
-                if (charClue1[i] == '-') {
-                    charClue6[cloud] = charClue[i];
-                    cloud++;
+        for (String words : clue) {
+            char[] wordFromTheList = words.toCharArray();                 //слово из списка
+            for (int i = 0; i < wordFromTheList.length; i++) {
+                if (serviceSymbols[i] == '+') {
+                    wordFromTheList[i] = '+';
                 }
             }
-
-            for (String words : clue) {
-                boolean bo = true;
-                char[] charClue7 = words.toCharArray();                             //слово из списка
-                char[] charClue8 = new char[charClue.length - numberOfGuessed];     //проверяемые символы
-                int cloud1 = 0;
-                for (int i = 0;  i < charClue7.length; i++) {
-                    if (charClue1[i] != '+') {
-                        charClue8[cloud1] = charClue7[i];
-                        cloud1++;
-                    }
-                }
-
-                for (int i = 0; i < charClue6.length; i++) {
-                    for (int j = 0; j < charClue8.length; j++) {
-                        if (charClue6[i] == charClue8[j]) {
-                            bo = false;
-                        }
-                    }
-                }
-
-                if (bo) {
-                    clue3.add(words);
-                }
+            if (containsAllChars(wordFromTheList, LettersToCheck)) {
+                wordsOfYellow.add(words);
             }
-
-            clue = clue3;
-            Wordle.printWriter.writeData("Создан список возможных вариантов ответов(по \"+\" и \"^\" и \"-\"): "
-                    + clue3.size() + " слов.");
-
-        } catch (NullPointerException e) {
-            System.err.println("Произошла ошибка NullPointer3.");
-        } catch (IndexOutOfBoundsException e) {
-            System.err.println("Произошла ошибка выхода за границы массива3.");
         }
+
+        Wordle.printWriter.writeData("Создан список возможных вариантов ответов(по \"+\" и \"^\"): "
+                + wordsOfYellow.size() + " слов.");
+
+        return wordsOfYellow;
+
+    }
+
+    private ArrayList<String> ChoiceByGrays(char[] wordSpelled, char[] serviceSymbols,
+                                            int numberOfGrays, int numbertOfGreen) {
+
+        ArrayList<String> wordsOfGrays = new ArrayList<>();                 //временный список проверенных по -
+        char[] missingLetters = new char[numberOfGrays];                         //символы которых нет
+        int cloud = 0;
+
+        for (int i = 0; i < serviceSymbols.length; i++) {
+            if (serviceSymbols[i] == '-') {
+                missingLetters[cloud] = wordSpelled[i];
+                cloud++;
+            }
+        }
+
+        for (String words : clue) {
+            boolean bo = true;
+            char[] wordFromTheList = words.toCharArray();                                       //слово из списка
+            char[] lettersBeingChecked = new char[wordSpelled.length - numbertOfGreen];         //проверяемые символы
+            int cloud1 = 0;
+            for (int i = 0; i < wordFromTheList.length; i++) {
+                if (serviceSymbols[i] != '+') {
+                    lettersBeingChecked[cloud1] = wordFromTheList[i];
+                    cloud1++;
+                }
+            }
+
+            for (int i = 0; i < missingLetters.length; i++) {
+                for (int j = 0; j < lettersBeingChecked.length; j++) {
+                    if (missingLetters[i] == lettersBeingChecked[j]) {
+                        bo = false;
+                    }
+                }
+            }
+
+            if (bo) {
+                wordsOfGrays.add(words);
+            }
+        }
+
+        Wordle.printWriter.writeData("Создан список возможных вариантов ответов(по \"+\" и \"^\" и \"-\"): "
+                + wordsOfGrays.size() + " слов.");
+
+        return wordsOfGrays;
     }
 
     public static boolean containsChar(char[] array, char target) {
